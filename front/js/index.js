ef674f05bf281c01de8c404fe98033f896dda34e
Ale <alejandraalvarado653@gmail.com>
2021-09-30 22:21:27 -0500

    Actualizacion del navbar y carrito en la barra de navegacion

--- front/js/index.js
+++ front/js/index.js
@@ -11,20 +11,26 @@ async function getProduct() {
 
 async function cargarDatos(pro){
     let products = document.getElementById("products");
-for (let i = 1; i < 20; i++) {
+for (let i = 1; i < 22; i++) {
 var titulo = pro.results[i].title
 var contenedor = document.createElement("div");
 contenedor.setAttribute("id", "p" + i);
 var imagen = document.createElement("img");
+
 imagen.setAttribute("id","imagen"+i);
 imagen.setAttribute("src", pro.results[i].thumbnail);
+var titulo = pro.results[i].title.split(" ", 6);
+console.log(typeof(titulo))
 let producto = `
-<div class="card" style="width: 18rem; margin-top: 20px;">
-<img src="`+pro.results[i].thumbnail+`" class="card-img-top">
+<div class="card mb-4 shadow-sm" style="width: 18rem; margin-top: 20px;">
+ <div class="card-header">
+ <h4 class="my-0 font-weight-bold" id="titulo"+i>`+titulo[0]+` `+titulo[1]+` `+titulo[2]+`</h4>
+    </div>
 <div class="card-body">
-    <h5 class="card-title" id="titulo"+i>`+pro.results[i].title+`</h5>
-    <p class="card-text">$`+pro.results[i].price+`</p>
-    <a href="#" class="btn btn-primary"> <i class="fas fa-cart-plus"></i></a>
+    <img src="`+pro.results[i].thumbnail+`" class="card-img-top">
+    
+    <h1 class="card-title pricing-card-title precio">$ <span class="">`+pro.results[i].price+`</span></h1>
+    <a href="#" class="btn btn-block btn-primary agregar-carrito" data-id="btn_`+i+`"> <i class="fas fa-cart-plus"></i></a>
 </div>
 </div>`;
 
