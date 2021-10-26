546d94366c8828c2c80ba9b8244ff032da47a0c1
Ale <alejandraalvarado653@gmail.com>
2021-10-25 21:10:08 -0500

    Vista de administrador

--- back/view/viewAdmin.js
+++ back/view/viewAdmin.js
@@ -1,5 +1,21 @@
+const productController = require('../controller/controllerProduct')
+const userController = require('../controller/controllerUser')
+
 module.exports=async (app)=>{
     app.get('/homeAdmin', async (req,res)=>{
-        res.render('admin')
+        var dataP = await productController.readP();
+        var dataU = await userController.readU();
+        res.render('admin',{dataP: dataP, dataU: dataU})
     })
+    app.get('/homeAdmin/newProduct', async (req,res)=>{
+        res.render('newProduct')
+    })   
+    app.post('/homeAdmin/newProduct',async(req,res)=>{
+        let product = req.body;
+        console.log(product)
+        let data = await productController.createP(product);
+        if(data){
+            res.redirect('/homeAdmin')
+        }
+    });
 }
